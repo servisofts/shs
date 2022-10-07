3649c4e6159e7c1815250c0fd8d7b8547c8cc948
rickypazd <rickypazd@hotmail.com>
2022-10-07 01:10:29 -0400

    Update backups.sh

--- backups.sh
+++ backups.sh
@@ -10,5 +10,5 @@ nombre=$(date +"%Y%m%d%H%M%S")_CasaGrande.sql
 
 pg_dump -U postgres -h 192.168.5.37 -p 5432 servisofts.CasaGrande > /u01/backups/$nombre
 
-curl -X POST --data-urlencode "payload={\"channel\": \"#backups\", \"username\": \"Servisofts SRL\", \"text\": \"Backup exitoso "" :smile:\", \"icon_url\": \"https://servisofts.com/img/servisofts.png\"}" https://hooks.slack.com/services/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
+curl -X POST --data-urlencode "payload={\"channel\": \"#backups\", \"username\": \"Servisofts SRL\", \"text\": \"Backup exitoso :smile:\", \"icon_url\": \"https://servisofts.com/img/servisofts.png\"}" https://hooks.slack.com/services/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
 
